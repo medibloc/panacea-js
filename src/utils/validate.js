706b1ccaac44556a578bb50c69b74d7fd2f04215
ggomma <ggomma@gmail.com>
2019-08-22 18:26:27 +0900

    Use objectPath module to validate data params

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,8 @@
+import objectPath from 'object-path';
+
 export const checkParams = (requiredParams = [], data = {}) => {
   requiredParams.forEach((param) => {
-    if (!data[param]) throw new Error(`\'${param}\' field is required`);
+    if (!objectPath.has(data, param)) throw new Error(`\'${param}\' field is required`);
   });
   return true;
 };

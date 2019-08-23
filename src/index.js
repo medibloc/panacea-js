c632addea2667285bab18c47b45c6086b7704e65
ggomma <ggomma@gmail.com>
2019-08-23 13:41:48 +0900

    Wrap codes to deploy

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Account from './account';
+import Client from './client';
+import Coin, { Fee } from './coin';
+import * as Message from './message';
+import Tx from './tx';
+import * as crypto from './crypto';
+import utils from './utils';
+
+export default {
+  Account,
+  Client,
+  Coin,
+  Fee,
+  Message,
+  Tx,
+  crypto,
+  utils,
+};

ec519d9a5a58efe4474141191af6ba7d0ba4a52e
ggomma <ggomma@gmail.com>
2019-08-23 11:03:27 +0900

    Add coin module test

---
+++ test/coin/coin.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import Coin from '../../src/coin';
+import { DEFAULT_DENOM } from '../../config';
+
+describe('coin', () => {
+  describe('constructor', () => {
+    it('generates default coin object', () => {
+      let coin = new Coin();
+      expect(coin.denom).to.be.eql(DEFAULT_DENOM);
+      expect(coin.amount).to.be.eql('0');
+
+      const amount = 123456789;
+      coin = new Coin(amount);
+      expect(coin.denom).to.be.eql(DEFAULT_DENOM);
+      expect(coin.amount).to.be.eql(`${amount}`).not.to.be.eql(amount);
+
+      const anotherDenom = 'test';
+      coin = new Coin(`${amount}${anotherDenom}`);
+      expect(coin.denom).to.be.eql(anotherDenom).not.to.be.eql(DEFAULT_DENOM);
+      expect(coin.amount).to.be.eql(`${amount}`).not.to.be.eql(amount);
+    });
+  });
+
+  describe('parseCoin', () => {
+    it('throws error if argument is invalid', () => {
+      expect(() => Coin.parseCoin(DEFAULT_DENOM)).to.throw();
+      expect(() => Coin.parseCoin(123456789)).to.throw();
+    });
+
+    it('generates object containing amount and denom fields', () => {
+      const amount = 123456789;
+      const coin = Coin.parseCoin(`${amount}${DEFAULT_DENOM}`);
+      expect(coin.amount).to.be.equal(`${amount}`);
+      expect(coin.denom).to.be.equal(DEFAULT_DENOM);
+    });
+  });
+});

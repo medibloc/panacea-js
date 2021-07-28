1eb19e321314c7c72c5b4a7cf5d87d0cc2f9777f
Youngjoon Lee <yjlee@medibloc.org>
2021-07-28 15:15:45 +0900

    fix: Discard `protodep` (#53)

--- protocgen.sh
+++ protocgen.sh
@@ -8,7 +8,14 @@ SRC_PATH="${SCRIPT_DIR}/third_party/proto"
 DST_PATH="${SCRIPT_DIR}/src/proto"
 
 # Fetch 3rd-party proto files
-protodep up -u
+PANACEA_CORE_VER_SHORT="2.0.1"
+PANACEA_CORE_VER="v${PANACEA_CORE_VER_SHORT}"
+
+mkdir -p ${SRC_PATH}
+curl https://codeload.github.com/medibloc/panacea-core/tar.gz/${PANACEA_CORE_VER} | \
+	tar -xz -C ${SRC_PATH} --strip=3 panacea-core-${PANACEA_CORE_VER_SHORT}/third_party/proto
+curl https://codeload.github.com/medibloc/panacea-core/tar.gz/${PANACEA_CORE_VER} | \
+	tar -xz -C ${SRC_PATH} --strip=2 panacea-core-${PANACEA_CORE_VER_SHORT}/proto
 
 # Compile only Panacea proto files
 mkdir -p ${DST_PATH}

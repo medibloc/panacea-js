33fd0760bd804312c3b9fc4824ebec422b79dfc6
gyuguen <gyuguen.jang@medibloc.org>
2022-08-16 18:21:27 +0900

    fix

---
+++ src/crypto/secp256k1.spec.ts
@@ -0,0 +1,19 @@
+import {Secp256k1 as CryptoSecp256k1, stringToPath, sha256} from "@cosmjs/crypto";
+import {panacead} from "../testutils";
+import {TextEncoder} from "util";
+import {Secp256k1} from "./secp256k1";
+
+describe("Secp256k1", () => {
+
+    it("parseMnemonicToPrivateKey", async () => {
+        const body = "testBody";
+        const hashedBody = sha256(new TextEncoder().encode(body));
+        const hdPath = stringToPath("m/44'/371'/0'/0/0");
+
+        const privateKey = await Secp256k1.parseMnemonicToPrivateKey(panacead.mnemonic, hdPath);
+        const {pubkey} = await CryptoSecp256k1.makeKeypair(privateKey);
+
+        const signature = await CryptoSecp256k1.createSignature(hashedBody, privateKey)
+        expect(CryptoSecp256k1.verifySignature(signature, hashedBody, pubkey)).toBeTruthy()
+    });
+});
--- src/crypto/secp256k1.ts
+++ src/crypto/secp256k1.ts
@@ -1,27 +1,27 @@
 import ecc from "secp256k1";
-import { randomBytes } from "crypto";
-import crypto_1 from "@cosmjs/crypto"
+import {randomBytes} from "crypto";
+import {Bip39, EnglishMnemonic, Slip10, Slip10Curve, Slip10RawIndex} from "@cosmjs/crypto"
 
 export class Secp256k1 {
-  static generatePrivateKey(): Uint8Array {
-    let privKey
-    do {
-      privKey = randomBytes(32)
-    } while (!ecc.privateKeyVerify(privKey));
-    return privKey;
-  }
+    static generatePrivateKey(): Uint8Array {
+        let privKey
+        do {
+            privKey = randomBytes(32)
+        } while (!ecc.privateKeyVerify(privKey));
+        return privKey;
+    }
 
-  static getPublicKeyCompressed(privKey: Uint8Array): Uint8Array {
-    return ecc.publicKeyCreate(privKey);
-  }
+    static getPublicKeyCompressed(privKey: Uint8Array): Uint8Array {
+        return ecc.publicKeyCreate(privKey);
+    }
 
-  static sign(data32: Uint8Array, privKey: Uint8Array): Uint8Array {
-    return ecc.ecdsaSign(data32, privKey).signature;
-  }
+    static sign(data32: Uint8Array, privKey: Uint8Array): Uint8Array {
+        return ecc.ecdsaSign(data32, privKey).signature;
+    }
 
-  static async parseMnemonicToPrivateKey(mnemonic: string, hdPath: readonly crypto_1.Slip10RawIndex[]): Promise<Uint8Array> {
-    const mnemonicChecked = new crypto_1.EnglishMnemonic(mnemonic)
-    const seed = await crypto_1.Bip39.mnemonicToSeed(mnemonicChecked, "")
-    return crypto_1.Slip10.derivePath(crypto_1.Slip10Curve.Secp256k1, seed, hdPath).privkey
-  }
+    static async parseMnemonicToPrivateKey(mnemonic: string, hdPath: readonly Slip10RawIndex[]): Promise<Uint8Array> {
+        const mnemonicChecked = new EnglishMnemonic(mnemonic)
+        const seed = await Bip39.mnemonicToSeed(mnemonicChecked, "")
+        return Slip10.derivePath(Slip10Curve.Secp256k1, seed, hdPath).privkey
+    }
 }

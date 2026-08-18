2975eab8091518db79675acabb183d8f11932443
Minkyoo Cha <minkyoo.cha@medibloc.org>
2026-08-18 15:34:28 +0900

    test: separate unit and integration suites

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
-// jest.config.js
+const integrationTestPaths = [
+  "/src/__tests__/group-signing-panacea-client.spec.ts",
+  "/src/__tests__/panacea-client.test.ts",
+  "/src/__tests__/signing-panacea-client.test.ts",
+];
+
+const integrationEnabled = process.env.PANACEAD_ENABLED === "true";
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    ...(integrationEnabled ? [] : integrationTestPaths),
+  ],
 };

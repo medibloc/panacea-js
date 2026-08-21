a6da60f55e7cdf84114bab2567d12f06fbefd7cf
Cha Minkyoo <9249679+cl9200@users.noreply.github.com>
2026-08-21 16:52:13 +0900

    fix(deps): migrate security-sensitive dependencies (#82)

--- src/__tests__/secp256k1.spec.ts
+++ src/__tests__/secp256k1.spec.ts
@@ -3,6 +3,7 @@ import {
   stringToPath,
   sha256,
 } from "@cosmjs/crypto";
+import { fromHex, toHex } from "@cosmjs/encoding";
 import { TextEncoder } from "util";
 import * as jose from "jose";
 import { Secp256k1 } from "../crypto";
@@ -42,6 +43,25 @@ describe("Secp256k1", () => {
     );
     expect(Secp256k1.uncompressPublicKey(pubKeyUncompressed)).toEqual(
       pubKeyUncompressed,
+    );
+  });
+
+  it("preserves public key and deterministic signature bytes", () => {
+    const privateKey = fromHex(
+      "0000000000000000000000000000000000000000000000000000000000000001",
+    );
+    const messageHash = fromHex(
+      "6afff7824c9e5f73775b6dd8b27b43e8cd808b02ac02e154ece5245d56bc5ad2",
+    );
+
+    expect(toHex(Secp256k1.getPublicKeyCompressed(privateKey))).toBe(
+      "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+    );
+    expect(toHex(Secp256k1.getPublicKeyUncompressed(privateKey))).toBe(
+      "0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8",
+    );
+    expect(toHex(Secp256k1.sign(messageHash, privateKey))).toBe(
+      "c92fb23091373730989a4fcdc34ecc5885fcf1607ec4732fba04fc4ccba7ef201a51bffadd15b344a80735099afe380603c8cbc8edf71f62cf671d26c328ecef",
     );
   });
 
